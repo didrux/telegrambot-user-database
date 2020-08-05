6db7e72f9a9e5f8817bf33ee4499d3e4e963a74c
didrux <60069950+didrux@users.noreply.github.com>
2020-08-05 18:59:47 +0300

    ability to send database to currently user

--- sendmsg.py
+++ sendmsg.py
@@ -1,7 +1,7 @@
 import telebot
 token = "1310155222:AAGRUfmrIomT9gPJgRCuto_5IYLCI1PhgOg"
 bot = telebot.TeleBot(token)
-
+adminchatid = "346037244" #look in database file
 
 joinedFile = open("db.txt", "r")
 joinedUsers = set ()
@@ -14,23 +14,20 @@ def startJoin(message):
     try:
         if not str(message.chat.id) in joinedUsers:
             joinedFile = open("db.txt", "a")
-            joinedFile.write(str(message.chat.id) + " " + message.from_user.username + "\n")
+            joinedFile.write(str(message.chat.id) + "\n")
             joinedUsers.add(str(message.chat.id))
     except TypeError:
         if not str(message.chat.id) in joinedUsers:
             joinedFile = open("db.txt", "a")
             joinedFile.write(str(message.chat.id) + "\n")
             joinedUsers.add(str(message.chat.id))
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Сhoose the currency you want to convert to", reply_markup=keyboard())
+
+
 
 @bot.message_handler(commands=['special'])
 def mess(message):
-    for user in joinedUsers:
-        bot.send_message(user, message.text[message.text.find(' '):])
-
-
-
+    f = open("db.txt", "rb")
+    bot.send_document(chat_id=adminchatid, data=f)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
